1f8a996ec124b490a8f9a5b851d01f45b2c722c4
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-03-25 11:29:38 +0100

    corrections

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,14 @@
-/** Import des librairies */
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { resolvers } from "./resolvers/cartoon.resolver";
 
-// const cartoons = [
-// 	{
-// 		id: 1,
-// 		name: "Les Mystérieuses Cités d'Or",
-// 		description:
-// 			"Esteban, un jeune garçon orphelin, part à la recherche des légendaires Cités d'Or en Amérique du Sud accompagné de Zia et Tao.",
-// 	},
-// 	{
-// 		id: 2,
-// 		name: "Ulysse 31",
-// 		description:
-// 			"Ulysse se perd dans l'espace avec son équipage et cherche à rentrer sur Terre tout en affrontant les dieux de l'Olympe.",
-// 	},
-// 	{
-// 		id: 3,
-// 		name: "Dragon Ball SUPER COOL",
-// 		description:
-// 			"Son Goku, un jeune garçon doté d'une force incroyable, part à la recherche des Dragon Balls, des boules de cristal magiques.",
-// 	},
-// ];
+import { getCartoons, getOneCartoonById } from "./resolvers/cartoon.resolver";
 
 // A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
 const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
   # This "Cartoon" type defines the queryable fields for every cartoon in our data source.
   type Cartoon {
     id: ID
@@ -33,24 +16,39 @@ const typeDefs = `#graphql
     description: String
   }
 
-  # The "Query" type is special: it lists all of the available queries
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "cartoons" query returns an array of zero or more Cartoons (defined above).
   type Query {
-    getCartoons: [Cartoon]
+    getCartoons: [Cartoon],
     getOneCartoonById(id: ID!): Cartoon,
   }
 `;
 
+// Resolvers define how to fetch the types defined in your schema.
+// This resolver retrieves books from the "books" array above.
+const resolvers = {
+	Query: {
+		getCartoons,
+		getOneCartoonById,
+	},
+};
+
 // The ApolloServer constructor requires two parameters: your schema
-// definition (les shema GrapQL composes des defintions de tous nos types de donnees, de nos types Querys et de Mutations)  and your set of resolvers (fonctions de resolution, la ou on met notre logique metiers).
+// definition and your set of resolvers.
 const server = new ApolloServer({
 	typeDefs,
 	resolvers,
 });
 
-/** Fonction auto appelee (evite la mise en constante) permettant de lancer le serveur */
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
 (async () => {
 	const { url } = await startStandaloneServer(server, {
 		listen: { port: 4000 },
 	});
-	console.log(`🚀 Server ready at: ${url}`);
+
+	console.log(`🚀  Server ready at: ${url}`);
 })();
--- src/resolvers/cartoon.resolver.ts
+++ src/resolvers/cartoon.resolver.ts
@@ -1,5 +1,9 @@
-import { default as cartoons } from "../../dataset.json";
 import type { Cartoon } from "../types/cartoon.type";
+import { default as cartoons } from "../../dataset.json";
+
+export const getCartoons = (): Cartoon[] => {
+	return cartoons;
+};
 
 type GetOneCartoonByIdArgs = {
 	id: string;
@@ -8,14 +12,6 @@ type GetOneCartoonByIdArgs = {
 export const getOneCartoonById = (
 	_: unknown,
 	args: GetOneCartoonByIdArgs,
-): Cartoon | undefined => {
-	return cartoons.find((cartoon) => cartoon.id === Number(args.id)) as Cartoon;
-};
-
-// This resolver retrieves books from the "books" array above.
-export const resolvers = {
-	Query: {
-		getCartoons: (): Cartoon[] => cartoons,
-		getOneCartoonById,
-	},
+): Cartoon => {
+	return cartoons.find((cartoon) => cartoon.id === +args.id) as Cartoon;
 };
