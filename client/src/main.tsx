3809863f33eaeb537f0097a0efefdd1f81704546
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-03-30 13:30:21 +0200

    :wrench: ApolloProvider set up

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ApolloProvider } from "@apollo/client";
+import client from "./services/apolloClient.ts";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
+	</StrictMode>,
+);
