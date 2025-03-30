4f33d12fbf5576fc019118dfd54eb6d91b484379
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-03-30 13:53:26 +0200

    :test_tube: first request tests in console

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,14 @@
+import { useQuery } from "@apollo/client";
 import "./App.css";
+import { GET_ALL_CARTOONS } from "./shemas/cartoon.schema";
 
 function App() {
+	const { loading, error, data } = useQuery(GET_ALL_CARTOONS);
+	console.log(data);
+
+	if (loading) return <p>Loading in progress...</p>;
+	if (error) return <p>There might be an error</p>;
+
 	return (
 		<>
 			<h1>My projetc</h1>
